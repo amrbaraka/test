b6246fabbe73279b4fa4aa9b54cc7300df863413
amrbaraka <amrbaraka84@hotmail.com>
2025-04-29 16:27:35 +0100

    Added echo $REPLICAS echo $IMAGE

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Load variables from ConfigMap mounted file
 . /configs/vars.env
+echo $REPLICAS
+echo $IMAGE
 #Substitute the variables in app.template.yaml and output to app.yaml
 envsubst < /repo/backend/app.template.yaml > /tmp/app.yaml
 # Apply the modified YAML
