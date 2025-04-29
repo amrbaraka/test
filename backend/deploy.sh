165aaa766337184f4f57cb24c98f24d8efdb6fe6
amrbaraka <amrbaraka84@hotmail.com>
2025-04-29 14:31:08 +0100

    /test/backend/app.template.yaml > /repo/backend/app.template.yaml

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,6 +2,6 @@
 # Load variables from ConfigMap mounted file
 . /configs/vars.env
 #Substitute the variables in app.template.yaml and output to app.yaml
-envsubst < /test/backend/app.template.yaml > /tmp/app.yaml
+envsubst < /repo/backend/app.template.yaml > /tmp/app.yaml
 # Apply the modified YAML
 kubectl apply -f /tmp/app.yaml
