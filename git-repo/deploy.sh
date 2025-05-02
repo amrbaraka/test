ab64734987a91fb023fcd31f06db9cff94d6940b
amrbaraka <amrbaraka84@hotmail.com>
2025-05-02 19:21:46 +0100

    Update path app template path to /test/git-repo/app.template.yaml

--- git-repo/deploy.sh
+++ git-repo/deploy.sh
@@ -6,6 +6,6 @@ set +a
 echo $REPLICAS
 echo $IMAGE
 #Substitute the variables in app.template.yaml and output to app.yaml
-envsubst < /repo/backend/app.template.yaml > /tmp/app.yaml
+envsubst < /test/git-repo/app.template.yaml > /tmp/app.yaml
 # Apply the modified YAML
 kubectl apply -f /tmp/app.yaml
